f8dfcb801af702d323beb878a23c51b0a419b1c0
Peter Yang <ipod4928@gmail.com>
2024-04-29 23:28:59 +0800

    fix word break issue

--- src/components/message.svelte
+++ src/components/message.svelte
@@ -1,19 +1,23 @@
-<script lang='ts'>
-  import {onMount} from 'svelte';
-  import type {message} from '../types/message';
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import type { message } from '../types/message';
 
-  import {currentFirestoreUser, getUserData} from '../firebase/firestore'
+	import { currentFirestoreUser, getUserData } from '../firebase/firestore';
 
-  export let message: message;
-
+	export let message: message;
 </script>
 
 {#if $currentFirestoreUser !== null}
-<div class="flex flex-col items-start whitespace-break-spaces">
-    <div class="text-white  {message.userid === $currentFirestoreUser.uid ? ' ml-auto':''}">{message.userName}</div>
-    <div class=" max-w-md px-4 py-2 rounded-2xl my-2  {message.userid === $currentFirestoreUser.uid ? 'bg-vintageBeige ml-auto':'bg-white'}">
-      {message.content}
-    </div>
-</div>
+	<div class="flex flex-col items-start whitespace-break-spaces break-all">
+		<div class="text-white {message.userid === $currentFirestoreUser.uid ? ' ml-auto' : ''}">
+			{message.userName}
+		</div>
+		<div
+			class=" max-w-md px-4 py-2 rounded-2xl my-2 {message.userid === $currentFirestoreUser.uid
+				? 'bg-vintageBeige ml-auto'
+				: 'bg-white'}"
+		>
+			{message.content}
+		</div>
+	</div>
 {/if}
-
