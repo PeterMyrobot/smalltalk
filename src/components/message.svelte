1e6f626267068c8e409b57d5cb4b996816d0ccbd
Peter Yang <ipod4928@gmail.com>
2024-04-30 00:01:01 +0800

    added style and prettify user name

--- src/components/message.svelte
+++ src/components/message.svelte
@@ -2,7 +2,8 @@
 	import { onMount } from 'svelte';
 	import type { message } from '../types/message';
 
-	import { currentFirestoreUser, getUserData } from '../firebase/firestore';
+	import { currentFirestoreUser } from '../firebase/firestore';
+	import capitalizeFirstChar from '../utils/capitalizeFirstChar';
 
 	export let message: message;
 </script>
@@ -10,7 +11,7 @@
 {#if $currentFirestoreUser !== null}
 	<div class="flex flex-col items-start whitespace-break-spaces break-all">
 		<div class="text-white {message.userid === $currentFirestoreUser.uid ? ' ml-auto' : ''}">
-			{message.userName}
+			{capitalizeFirstChar(message.userName.split('@')[0])}
 		</div>
 		<div
 			class=" max-w-md px-4 py-2 rounded-2xl my-2 {message.userid === $currentFirestoreUser.uid
